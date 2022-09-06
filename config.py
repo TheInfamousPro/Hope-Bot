8568f9ba738cb4e460a3c8d1db219d48cd207887
TheInfamousPro <96720512+TheInfamousPro@users.noreply.github.com>
2022-09-06 16:08:35 +0800

    Create config.py
    Created blacklist and other good stuff

---
+++ config.py
@@ -0,0 +1,10 @@
+blacklist = ["suicide","kill","jump out","jump off","run o","end my life","hate my life","hate life","fuck life","poison","shoot myself","commit suicide","want to die","wanna die","wish i was dead","wish I was dead","life sucks","fuck life","pointless","useless","hopeless","end my life","exit this world","depressed","my life is so miserable","my life is so sad","no point living","sick of life","sick of this world","hang myself","drug overdose","overdose myself","harm myself","what’s the point of life","what's the point of life","wth is the point of life","why am I still alive","why am i still alive","im sad","want to cut myself","wanna cut myself","tired of life","tired of this world","hate myself","good at nothing","suck at everything","bad at everything","crying right now","stab myself","better if I wasn’t living","better if i wasn’t living","better if I wasn't living","better if i wasn't living","better if I wasn’t alive","better if i wasn’t alive","better if I wasn't alive","better if i wasn't alive","wish I wasn’t living","wish i wasn’t living","wish I wasn't living","wish i wasn't living","wish I wasn’t alive","wish i wasn’t alive","wish I wasn't alive","wish i wasn't alive","kms","sick of life",'helpless']
+
+
+Comfort = ["I want to let you know that you’re not alone","I’m really sorry that you’re going through all of this — I really understand how hard it can be","I know that life can be really difficult at times… But don’t fret — I’m here to help","I really understand how you’re feeling right now… Don’t worry — we’re in this together","I want you to know that there is light at the end of the tunnel — not all is lost"]
+
+
+Greeting = ["Hey", "Howdy", "Hey there", "Hi", "Hello"]
+
+
+Sign = ["It seems like you are showing signs of suicidal ideation","It looks like you’re suffering from depression","It looks like you’re going through some tough times","You seem to be having suicidal thoughts","It seems like you are showing some suicidal thoughts"]
